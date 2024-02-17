21680baaab6701e73b89d303f4ddccc5d91c6910
LEONHARD LEUNG <2230618@slu.edu.ph>
2024-02-18 01:41:39 +0800

    fix: Fix UI not displaying
    - This commit addresses an issue regarding the server interface not loading when the server starts.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import server.controller.ServerController;
 import server.model.ServerModel;
@@ -15,32 +16,42 @@ import java.io.IOException;
 public class Server extends Application {
     private static final int PORT = 2000;
     private static final int THREAD_POOL_SIZE = 20;
+    private static ServerModel model;
+    private static ServerView view;
+    private static ServerController controller;
 
     public static void main(String[] args) {
-       launch(args);
+        launch(args);
     } // end of main
 
     @Override
     public void start(Stage stage) {
+        model = new ServerModel();
+        view = new ServerView(stage);
+        view.runInterface();
+        controller = new ServerController(model, view);
+
+        new Thread(() -> {
+            try {
+                startServer();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    } // end of start
+
+    private void startServer() throws IOException {
         try (ServerSocket server = new ServerSocket(PORT)) {
             System.out.println("Server listening on port " + PORT);
 
             ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-            ServerModel model = new ServerModel();
-            ServerView view = new ServerView(stage);
-            view.runInterface();
-            ServerController controller = new ServerController(model, view);
-
-
-            //todo, there is a problem here, the view.getUIExit() method returns false that is why it dont open its socket for incoming client connection
             while (true) {
                 Socket client = server.accept();
+                System.out.println("Client connected");
                 controller.setClientSocket(client);
                 executorService.submit(controller::run);
             }
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
         }
-    } // end of start
+    }
 } // end of Server class
